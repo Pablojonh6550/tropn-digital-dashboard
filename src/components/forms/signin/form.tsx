960a7ffb8faf8ad6030a597e07043ef67bec5eaf
Pablojonh6550 <pablojonh6550@gmail.com>
2025-06-08 15:50:07 -0300

    feat: add login form

--- src/components/forms/signin/form.tsx
+++ src/components/forms/signin/form.tsx
@@ -1,8 +1,33 @@
+import React from "react";
+import "./style.scss";
+
+import Input from "@/components/input/input";
+import Button from "@/components/button/button";
+
 const SignInForm = () => {
     return (
-        <div>
-            <h1>Sign In</h1>
-        </div>
+        <section className="wrapper">
+            <div className="login-container">
+                <div className="form-container">
+                    <div className="form-content">
+                        <img src="logo.png" alt="Logo" width={161} />
+                        <div className="title-container">
+                            <h2>Bem-vindo de volta</h2>
+                            <span className="sub-title">Entre com sua conta para acessar o painel.</span>
+                        </div>
+                        <div className="form">
+                            <Input />
+                            <Input />
+                            <Button>Enviar</Button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="image-section">
+                    <img src="assets/monitoring.png" alt="icon" />
+                </div>
+            </div>
+        </section>
     )
 }
 
