be5c809e703a1304529c98158c2b3920f8993adf
Pablojonh6550 <pablojonh6550@gmail.com>
2025-06-08 15:49:45 -0300

    feat: add input component

---
+++ src/components/input/input.tsx
@@ -0,0 +1,11 @@
+import "./style.scss";
+const Input = () => {
+    return (
+        <div className="input-container">
+            <label htmlFor="input">E-mail</label>
+            <input type="text" placeholder="Placeholder" />
+        </div>
+    )
+}
+
+export default Input;
